cfe88e3fd63a3e5afb666977a95e53e5d6599bd2
Manene <juniormanene@gmail.com>
2025-06-10 13:12:50 +0200

    styles

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -15,12 +15,19 @@ const Colors = {
     dark: '#CC5500',
   },
   success: '#34C759',
-  error: '#FF3B30',
+  error: {
+    default: '#FF3B30',
+    light: '#FFDEDE',
+  },
   warning: '#FF9500',
   neutral: {
     white: '#FFFFFF',
     light: '#F5F5F5',
+    medium: '#999999',
     dark: '#666666',
+    lighter: '#E0E0E0',
+    lightest: '#FAFAFA',
+    black: '#000000',
   },
   transparent: 'transparent',
 } as const;
